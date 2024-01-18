344fb501879a73c798e4b1d59dc25e6861db7f32
Mobin Anooshi <mobinanooshi2003@gmail.com>
2024-01-18 17:16:39 +0330

    login and log out class

--- A/account/urls.py
+++ A/account/urls.py
@@ -4,5 +4,7 @@ from account import views
 
 app_name="account"
 urlpatterns = [
-    path('register/',views.UserRegisterView.as_view(),name='user_register')
+    path('register/',views.UserRegisterView.as_view(),name='user_register'),
+    path('login/', views.UserLoginView.as_view(),name='user_login'),
+    path('logout/',views.UserLogoutView.as_view(),name='user_logout')
 ]
--- A/account/views.py
+++ A/account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from account.forms import UserRegistretionForm
+from account.forms import UserRegistretionForm , UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate , login , logout
 # Create your views here.
 
 
@@ -23,3 +24,29 @@ class UserRegisterView(View):
             return redirect('home:home')
         else:
             return render(request , self.template_name,{'form':form})
+        
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+
+    def get(self , request):
+        form = self.form_class
+        return render(request , self.template_name , {'form':form})
+    
+    def post(self , request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request , username=cd['username'] , password=cd['password'])
+            if user is not None:
+                login(request , user)
+                messages.success(request , 'you login is successfully ','success')
+                return redirect('home:home')
+            messages.error(request , 'username or password is wrong','warning')
+        return render(request , self.template_name , {'form':form})
+    
+class UserLogoutView(View):
+    def get(self ,request):
+        logout(request)
+        messages.success(request , 'you logout is successfully ','success')
+        return redirect('home:home')
