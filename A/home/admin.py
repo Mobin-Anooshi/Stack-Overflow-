442c71fdeb4fbbd0311a672b7f7c17dc9a150e1d
Mobin Anooshi <mobinanooshi2003@gmail.com>
2024-01-20 12:07:54 +0330

    create user prtofile and customizing admin

--- A/home/admin.py
+++ A/home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from home.models import Post
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user','slug','updated',)
+    search_fields = ('slug','body')
+    list_filter = ('created',)
+    prepopulated_fields={'slug':('body',)}
+    raw_id_fields = ('user',)
+
+
+
+
+admin.site.register(Post,PostAdmin)
